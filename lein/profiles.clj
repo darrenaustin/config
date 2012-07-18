7783362d875c0056bb71fb3e36d26fa28a09bb69
Darren Austin <darren@darrenaustin.org>
2012-07-18 08:10:57 -0700

    Tried adding kibit to my lein profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,5 @@
 {:user {:plugins [[lein-difftest "1.3.7"]
                   [lein-marginalia "0.7.0"]
                   [lein-pprint "1.1.1"]
-                  [lein-swank "1.4.4"] ]}}
+                  [lein-swank "1.4.4"]
+                  [jonase/kibit "0.0.3"]]}}
